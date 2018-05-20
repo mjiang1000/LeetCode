76e7c524507e6d2e58c97667c1b6e8f5235c6a3f
jiangmin <1027706648@qq.com>
2018-05-20 17:09:34 +0800

    changed nothing

--- solutions/divide-two-integer.js
+++ solutions/divide-two-integer.js
@@ -13,3 +13,26 @@
 //   while ()
 // };
 
+/**
+ * @param {number} dividend
+ * @param {number} divisor
+ * @return {number}
+ */
+
+/**一个方法是一直做减法，然后计数，但是提交之后显示超时，
+* 一种解法，利用位运算， 左移一位相当于乘以2，
+* 是任何一个整数可以表示成以2的幂为底的一组基的线性组合，
+* 即num=a_0*2^0+a_1*2^1+a_2*2^2+…+a_n*2^n。基于以上这个公式我们先让除数左移直到大于被除数之前得到一个最大的基n的值，
+* 说明被除数中至少包含2^n个除数，然后减去这个基数，再依次找到n-1，…，1的值。将所有的基数相加即可得到结果。
+*/
+var divide = function(dividend, divisor) {
+  
+
+  return average
+
+};
+
+// console.log(divide(10,3))
+var d = Date.now()
+console.log(divide(2147483646,2)==2147483646/2 ,
+		Date.now() - d)
